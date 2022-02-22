b5ec1a911fe4e3c1de67352ea6fd04eab039faf2
Chris Norman <chris.norman2@googlemail.com>
2022-02-22 20:55:20 +0000

    Update tests.

--- test/src/pack_file_reader_test.dart
+++ test/src/pack_file_reader_test.dart
@@ -6,19 +6,19 @@ import 'package:test/test.dart';
 
 import '../reader_writer.dart';
 
+final outputFile = File('test_src.data');
+
 /// Get a read and a writer.
 Future<ReaderWriter> getReaderWriter(List<File> files) async {
   final writer = PackFileWriter(files);
-  final file = File('test_src.data');
-  await writer.write(file);
-  final randomAccessFile = await file.open();
+  await writer.write(outputFile);
+  final randomAccessFile = await outputFile.open();
   final reader = PackFileReader(randomAccessFile);
   return ReaderWriter(reader: reader, writer: writer);
 }
 
 void main() {
   final files = Directory('test/src').listSync().whereType<File>().toList();
-
   group(
     'PackFileReader',
     () {
@@ -53,6 +53,7 @@ void main() {
               reason: '$name did not match',
             );
           }
+          reader.randomAccessFile.closeSync();
         },
       );
     },
